0347136b05450ae810a0c24840213cf3bbe7ba36
Kostya Cholak <kostya.cholak@anvileight.com>
2020-09-08 17:30:54 +0300

    Add debugger

--- app/js/componentUpdates.js
+++ app/js/componentUpdates.js
@@ -4,16 +4,24 @@ let lastTick = new Date;
 let ticksPerSecond = 0;
 
 let pauseSimulation = false;
+let stepSimulation = false;
 let updates;
+let debug = false;
 
 
 function tick() {
     const start = new Date;
     updates = 0;
 
-    while(updateQueue.length > 0 && updates < 10000 && !pauseSimulation) {
+    while(updateQueue.length > 0 && updates < 10000 && (!pauseSimulation || stepSimulation)) {
         updateQueue.splice(0,1)[0]();
         ++updates;
+        stepSimulation = false;
+    }
+
+    if (stepSimulation) {
+        notifications.push("Debug: No more updates!");
+        stepSimulation = false;
     }
 
     ticksPerSecond = 1000 / (new Date - lastTick);
